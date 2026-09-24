d6eca557de17f534aa7ff6906a057a08a93545fa
adampollack <adampollack@gmail.com>
2026-09-24 00:34:35 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+window.FIREBASE_CONFIG = {
+  apiKey: "YOUR_ACTUAL_API_KEY",
+  authDomain: "psccplanner.firebaseapp.com",
+  databaseURL: "https://psccplanner-default-rtdb.firebaseio.com",
+  projectId: "psccplanner",
+  storageBucket: "psccplanner.firebasestorage.app",
+  messagingSenderId: "484397250103",
+  appId: "1:484397250103:web:aa6bba14afc44bdb52b1b2"
+};
